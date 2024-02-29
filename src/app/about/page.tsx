6bf041d4fa04094bc016f2660ec36e6c06871f4a
cheezeman <pksam5046@naver.com>
2024-02-29 13:54:36 +0900

    Add: Header, Footer 추가+스타일링

---
+++ src/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function aboutPage() {
+  return <div>어바웃페이지</div>;
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <p className="bg-amber-900 text-white text-sm text-center">
+      {"Welcome to my blog | ⓒ All Rights Reserved."}
+    </p>
+  );
+}
