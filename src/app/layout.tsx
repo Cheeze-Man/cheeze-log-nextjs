207221089498cc81b71a0295ff37a5c2e35a9bbb
cheezeman <pksam5046@naver.com>
2024-03-01 13:31:02 +0900

    Design: Post Card&Grid styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
-import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import "./globals.css";
 
 const sans = Open_Sans({ subsets: ["latin"] });
 
@@ -18,10 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className={sans.className}>
-      <body className="flex flex-col w-full max-w-screen-2xl mx-auto">
-        <Header />
-        <main className="grow">{children}</main>
-        <Footer />
+      <body>
+        <div className="flex flex-col w-full max-w-screen-2xl mx-auto">
+          <Header />
+          <main className="grow">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
--- src/components/FeaturedPosts.tsx
+++ src/components/FeaturedPosts.tsx
@@ -1,12 +1,12 @@
-import { getAllPosts } from "@/service/posts";
+import { getFeaturedPosts } from "@/service/posts";
 import PostsGrid from "./PostsGrid";
 
 export default async function FeaturedPosts() {
-  const posts = await getAllPosts();
+  const posts = await getFeaturedPosts();
 
   return (
     <section>
-      <h2>게시물</h2>
+      <h2 className="text-2xl font-bold my-4">추천 게시물</h2>
       <PostsGrid posts={posts} />
     </section>
   );
