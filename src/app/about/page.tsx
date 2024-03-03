c4bb04e18bed5b8c65ec55ba2ba360056f1c4016
cheezeman <pksam5046@naver.com>
2024-03-03 17:42:38 +0900

    Design: Contact page 기본 스타일링

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,29 +1,45 @@
 import Hero from "@/components/Hero";
+import Link from "next/link";
 
-const TITLE_CLASS = "text-4xl font-bold text-gray-800 mb-1 cursor-default";
-const DIV_CLASS = "transition-all hover:scale-110";
+const TITLE_CLASS = "text-4xl font-bold text-amber-900 mb-1 cursor-default";
+const LINK_CLASS = "underline decoration-wavy";
 const P_CLASS = "text-lg font-semibold text-gray-700 tracking-tight";
 
 export default function aboutPage() {
   return (
     <article className="flex flex-col h-full">
       <Hero />
-      <section className="flex flex-col bg-amber-100 shadow-lg p-8 m-8 text-center justify-evenly grow">
-        <div className={DIV_CLASS}>
-          <h2 className={TITLE_CLASS}>Who Am I?</h2>
+      <section className="flex flex-col gap-10 bg-amber-100 shadow-lg p-8 m-8 text-center grow">
+        <div>
+          <h2 className={TITLE_CLASS}>Who Am I ?</h2>
           <p className={P_CLASS}>
             도전적인 프론트엔드 개발자
             <br />
             나와 서비스의 끝없는 발전을 갈구하는 개발자
           </p>
         </div>
-        <div className={DIV_CLASS}>
+        <div>
+          <h2 className={TITLE_CLASS}>My Links</h2>
+          <p className={P_CLASS}>
+            <Link className={LINK_CLASS} href="https://github.com/Cheeze-Man">
+              Git hub
+            </Link>
+            <br />
+            <Link
+              className={LINK_CLASS}
+              href="https://blog.naver.com/cheeze-man"
+            >
+              Naver blog (portfolio)
+            </Link>
+          </p>
+        </div>
+        <div>
           <h2 className={TITLE_CLASS}>My Career</h2>
           <p className={P_CLASS}>
             {"Amber Road <internship> (2023.09 - 2023.10)"}
           </p>
         </div>
-        <div className={DIV_CLASS}>
+        <div>
           <h2 className={TITLE_CLASS}>My Skills</h2>
           <p className={P_CLASS}>
             HTML, CSS, JavaScript
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,43 @@
+import ContactForm from "@/components/ContactForm";
+import { SiGithub, SiNaver } from "react-icons/si";
+
+const LINKS = [
+  {
+    title: "github",
+    icon: <SiGithub />,
+    url: "https://github.com/Cheeze-Man",
+  },
+  {
+    title: "naverblog",
+    icon: <SiNaver />,
+    url: "https://blog.naver.com/cheeze-man",
+  },
+];
+
 export default function ContactPage() {
-  return <div>컨택페이지</div>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+      <p className="italic text-lg tracking-tighter">pksam5046@naver.com</p>
+      <ul className="flex gap-6 my-2">
+        {LINKS.map(({ title, icon, url }, i) => (
+          <a
+            className={`text-5xl transition-all hover:scale-110 ${
+              title === "naverblog"
+                ? "hover:text-green-500"
+                : "hover:text-amber-500"
+            }`}
+            key={`${i}${url}`}
+            href={url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {icon}
+          </a>
+        ))}
+      </ul>
+      <h2 className="text-3xl font-bold my-8">Or Send me an email</h2>
+      <ContactForm />
+    </section>
+  );
 }
