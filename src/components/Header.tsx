326d0a1f02df97d2b0d6c3324010755fea555555
cheezeman <pksam5046@naver.com>
2024-03-04 01:02:35 +0900

    Modify: Header.tsx 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,29 @@
 import Link from "next/link";
 
+const LINK_CLASS =
+  "transition-all hover:text-amber-500 hover:border-b-2 hover:border-b-amber-500";
+
 export default function Header() {
   return (
     <header className="flex justify-between item-center p-4">
       <Link href="/">
-        <h1 className="text-4xl font-extrabold">{"Stacking Cheeze"}</h1>
+        <h1 className="italic text-4xl font-extrabold transition-all hover:text-amber-500">
+          {"Stacking Cheeze 🧀"}
+        </h1>
       </Link>
       <nav className="flex gap-4 items-center font-semibold">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/posts">Posts</Link>
-        <Link href="/contact">Contact</Link>
+        <Link className={LINK_CLASS} href="/">
+          Home
+        </Link>
+        <Link className={LINK_CLASS} href="/about">
+          About
+        </Link>
+        <Link className={LINK_CLASS} href="/posts">
+          Posts
+        </Link>
+        <Link className={LINK_CLASS} href="/contact">
+          Contact
+        </Link>
       </nav>
     </header>
   );
