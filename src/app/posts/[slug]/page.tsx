808e96a5c11787b9a8bc090afdec232e787464fa
cheezeman <pksam5046@naver.com>
2024-03-03 14:51:29 +0900

    Add: post detail page - Markdown viewer

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,9 @@
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { AiTwotoneCalendar } from "react-icons/ai";
 import { getPostData } from "@/service/posts";
+import MarkDownViewer from "@/components/MarkdownViewer";
+import Image from "next/image";
 
 type Props = {
   params: {
@@ -6,12 +11,27 @@ type Props = {
   };
 };
 export default async function PostPage({ params: { slug } }: Props) {
-  const post = await getPostData(slug);
+  const { title, description, date, path, content } = await getPostData(slug);
 
   return (
-    <>
-      <h1>{post.title}</h1>
-      <pre>{post.content}</pre>
-    </>
+    <article className="rounded-2xl overflow-hidden bg-amber-100 shadow-lg m-4">
+      <Image
+        className="w-full h-1/5 max-h-[500px]"
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+      />
+      <section className="flex flex-col p-4">
+        <div className="flex items-center self-end text-amber-600">
+          <AiTwotoneCalendar />
+          <p className="text-xl font-semibold ml-2">{date.toString()}</p>
+        </div>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-xl font-bold">{description}</p>
+        <div className="w-full border-2 border-amber-600 mt-4 mb-8" />
+        <MarkDownViewer content={content} />
+      </section>
+    </article>
   );
 }
